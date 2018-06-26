26bd8c711556302d62fbba7594986320b253a480
Jim Lawson <ucbjrl@berkeley.edu>
2018-06-26 10:46:16 -0700

    Move poorly placed COMMIT_RANGE setting code.

--- .circleci/run_formal_checks.sh
+++ .circleci/run_formal_checks.sh
@@ -8,13 +8,13 @@ fi
 
 DUT=$1
 
+# Extract the x...y part of "The GitHub or Bitbucket URL to compare commits of a build."
+COMMIT_RANGE=`basename $CIRCLE_COMPARE_URL`
+
 # Run formal check only for PRs
 if [ $CIRCLE_PULL_REQUEST = "" ]; then
   echo "Not a pull request, no formal check"
   exit 0
-# Extract the x...y part of "The GitHub or Bitbucket URL to compare commits of a build."
-printenv
-COMMIT_RANGE=`basename $CIRCLE_COMPARE_URL`
 # Skip chisel tests if the commit message says to
 elif git log --format=%B --no-merges $COMMIT_RANGE | grep '\[skip formal checks\]'; then
   echo "Commit message says to skip formal checks"
