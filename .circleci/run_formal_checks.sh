0f864ebbf632290d1acf4a693bd74976a12fb626
Jim Lawson <ucbjrl@berkeley.edu>
2018-06-26 09:34:36 -0700

    Test enables at a higher level; use master branch for regression tests. Check RUN_CHISEL_TESTS and RUN_FORMAL_CHECKS in config.yml Since CircleCI doesn't directly indicate the destinaion branch for a PR, run formal regression checks against master.

--- .circleci/run_formal_checks.sh
+++ .circleci/run_formal_checks.sh
@@ -9,24 +9,23 @@ fi
 DUT=$1
 
 # Run formal check only for PRs
-if $RUN_FORMAL_CHECKS ; then
-  if [ $CIRCLE_PULL_REQUEST = "" ]; then
-    echo "Not a pull request, no formal check"
-    exit 0
-  elif git log --format=%B --no-merges $CIRCLE_BRANCH..HEAD | grep '\[skip formal checks\]'; then
-    echo "Commit message says to skip formal checks"
-    exit 0
-  else
-    # $CIRCLE_BRANCH is branch targeted by PR
-    # Checkout PR target so that we have it
-    # Then return to previous branch so HEAD points to the commit we're testing
-    git remote set-branches origin $CIRCLE_BRANCH && git fetch
-    git checkout $CIRCLE_BRANCH
-    git checkout -
-    cp regress/$DUT.fir $DUT.fir
-    ./scripts/formal_equiv.sh HEAD $CIRCLE_BRANCH $DUT
-  fi
+if [ $CIRCLE_PULL_REQUEST = "" ]; then
+  echo "Not a pull request, no formal check"
+  exit 0
+elif git log --format=%B --no-merges $CIRCLE_BRANCH..HEAD | grep '\[skip formal checks\]'; then
+  echo "Commit message says to skip formal checks"
+  exit 0
 else
-    echo "Not running formal check"
-    exit 0
+  # Unlike Travis, CircleCI doesn't directly indicate the destination branch
+  #  of the pull request.
+  # For the purposes of regression testing, it seems we lose nothing by
+  #  testing against master.
+  # Checkout master branch so that we have it
+  # Then return to previous branch so HEAD points to the commit we're testing
+  REGRESSION_BRANCH=master
+  git remote set-branches origin $REGRESSION_BRANCH && git fetch
+  git checkout $REGRESSION_BRANCH
+  git checkout -
+  cp regress/$DUT.fir $DUT.fir
+  ./scripts/formal_equiv.sh HEAD $REGRESSION_BRANCH $DUT
 fi
