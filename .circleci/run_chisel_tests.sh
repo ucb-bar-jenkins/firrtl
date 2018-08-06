99d7ba030d0cfc85365adff62614032a60a74add
ucb-bar-jenkins <ucb-bar-jenkins@users.noreply.github.com>
2018-08-06 13:48:02 -0700

    Don't copy utils Jar
    Assume the correct version has been published in the workspace.

--- .circleci/run_chisel_tests.sh
+++ .circleci/run_chisel_tests.sh
@@ -9,9 +9,6 @@ else
   # We assume the following has been done elsewhere
   #  sbt $SBT_ARGS assembly publishLocal
   git clone https://github.com/ucb-bar/chisel3.git
-  mkdir -p chisel3/lib
-  ls -l . utils/bin/firrtl.jar
-  cp utils/bin/firrtl.jar chisel3/lib
   cd chisel3
   sbt $SBT_ARGS "set concurrentRestrictions in Global += Tags.limit(Tags.Test, 2)" clean test
 fi
